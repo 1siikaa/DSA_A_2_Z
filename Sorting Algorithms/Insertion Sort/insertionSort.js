dbf9d308ca36a626e1aef987f2bc4aa3fcac3892
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2026-02-12 22:30:02 +0530

    dsa

---
+++ Sorting Algorithms/Insertion Sort/insertionSort.js
@@ -0,0 +1,18 @@
+// Take an element & place it in its current order.
+
+function insertionSortAsc(arr, n) {
+  for (let i = 0; i <= n - 1; i++) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      let temp = arr[j - 1];
+      arr[j - 1] = arr[j];
+      arr[j] = temp;
+      j--;
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSortAsc([5, 21, 0, 9, 5, 7, 6], 7));
+// Worst and Average = O(n^2)
+// Best case = O(n)
